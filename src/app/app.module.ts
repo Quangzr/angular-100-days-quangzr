905d39d6143b8f8ee1acb65db928d3b0ebe0fbc2
Quangzr <82323489+Quangzr@users.noreply.github.com>
2022-12-14 16:20:56 +0700

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { Day3DataEventBindingComponent } from './day3-data-event-binding/day3-data-event-binding.component';
 import { Day4NgIfComponent } from './day4-ng-if/day4-ng-if.component';
 import { Day5NgForComponent } from './day5-ng-for/day5-ng-for.component';
+import { Day6ClassStyleBindingComponent } from './day6-class-style-binding/day6-class-style-binding.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
@@ -15,6 +16,7 @@ import { Day5NgForComponent } from './day5-ng-for/day5-ng-for.component';
     Day3DataEventBindingComponent,
     Day4NgIfComponent,
     Day5NgForComponent,
+    Day6ClassStyleBindingComponent,
   ],
   bootstrap: [AppComponent],
 })
---
+++ src/app/day6-class-style-binding/day6-class-style-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-day6-class-style-binding',
+  templateUrl: './day6-class-style-binding.component.html',
+  styleUrls: ['./day6-class-style-binding.component.css']
+})
+export class Day6ClassStyleBindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
