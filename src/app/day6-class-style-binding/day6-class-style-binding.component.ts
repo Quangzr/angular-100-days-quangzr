7bbc99ed3b7598b4a5fa15a0f831c2fa76c14f89
Quangzr <82323489+Quangzr@users.noreply.github.com>
2022-12-14 17:15:25 +0700

    5 files modified

--- src/app/day6-class-style-binding/day6-class-style-binding.component.ts
+++ src/app/day6-class-style-binding/day6-class-style-binding.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-day6-class-style-binding',
   templateUrl: './day6-class-style-binding.component.html',
-  styleUrls: ['./day6-class-style-binding.component.css']
+  styleUrls: ['./day6-class-style-binding.component.css'],
 })
 export class Day6ClassStyleBindingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  isDanger = false;
+
+  isWarning = false;
+
+  classTest = 'danger-color warning-color';
+
+  styleTest = 'height: 50px';
+
+  changeDanger() {
+    this.isDanger = !this.isDanger;
   }
 
+  changeWarning() {
+    this.isWarning = !this.isWarning;
+  }
 }
