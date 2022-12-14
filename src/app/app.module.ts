32b8defab697d3c8352299b1ebb167f9d2bd82bd
Quangzr <82323489+Quangzr@users.noreply.github.com>
2022-12-14 16:16:08 +0700

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { Day4NgIfComponent } from './day4-ng-if/day4-ng-if.component';
 import { Day3DataEventBindingComponent } from './day3-data-event-binding/day3-data-event-binding.component';
+import { Day4NgIfComponent } from './day4-ng-if/day4-ng-if.component';
+import { Day5NgForComponent } from './day5-ng-for/day5-ng-for.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   // cần khai báo thêm các component vào phần declarations để có thể sử dụng
   declarations: [
     AppComponent,
-    Day4NgIfComponent,
     Day3DataEventBindingComponent,
+    Day4NgIfComponent,
+    Day5NgForComponent,
   ],
   bootstrap: [AppComponent],
 })
