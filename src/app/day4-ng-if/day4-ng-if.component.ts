83479285ffa70b119b77e60893f5f644a719027e
Quangzr <82323489+Quangzr@users.noreply.github.com>
2022-12-14 14:43:42 +0700

    3 files modified

--- src/app/day4-ng-if/day4-ng-if.component.ts
+++ src/app/day4-ng-if/day4-ng-if.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-day4-ng-if',
   templateUrl: './day4-ng-if.component.html',
-  styleUrls: ['./day4-ng-if.component.css']
+  styleUrls: ['./day4-ng-if.component.css'],
 })
 export class Day4NgIfComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  age = 17;
+
+  increaseAge() {
+    this.age = this.age + 1;
   }
 
+  decreaseAge() {
+    this.age = this.age - 1;
+  }
 }
