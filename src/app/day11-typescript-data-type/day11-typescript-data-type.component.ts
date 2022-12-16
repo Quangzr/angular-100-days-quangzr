c96df50abf6d97d63cb9eb2094908945cfc4af27
Quangzr <82323489+Quangzr@users.noreply.github.com>
2022-12-16 14:12:08 +0700

    1 file modified

--- src/app/day11-typescript-data-type/day11-typescript-data-type.component.ts
+++ src/app/day11-typescript-data-type/day11-typescript-data-type.component.ts
@@ -11,17 +11,17 @@ export class Day11TypescriptDataTypeComponent implements OnInit {
   ngOnInit() {}
 
   //Các kiểu dữ liệu trong TypeScript
-  let someString: string; // string, char
-  let someNumber: number; // float, double, int
-  let someBoolean: boolean; // true, false
-  let something: any; // có thể gán sang cho bất kỳ kiểu dữ liệu nào khác
-  let someStringArray: string[]; // tương tự cho number[], boolean[], any[]
-  let someObject: object;
-  let someNull: null;
-  let someUndefined: undefined;
-  let someUnknown: unknown;
-  let someNever: never; // ví dụ như một hàm throw exception
-  let someTuple: [string, number];
-  let someVoidFunction: () => void; // một hàm không trả về giá trị gì sau khi thực thi
-  let someFunction: () => string; // một hàm trả về giá trị có type "string" sau khi thực thi
+  // let someString: string; // string, char
+  // let someNumber: number; // float, double, int
+  // let someBoolean: boolean; // true, false
+  // let something: any; // có thể gán sang cho bất kỳ kiểu dữ liệu nào khác
+  // let someStringArray: string[]; // tương tự cho number[], boolean[], any[]
+  // let someObject: object;
+  // let someNull: null;
+  // let someUndefined: undefined;
+  // let someUnknown: unknown;
+  // let someNever: never; // ví dụ như một hàm throw exception
+  // let someTuple: [string, number];
+  // let someVoidFunction: () => void; // một hàm không trả về giá trị gì sau khi thực thi
+  // let someFunction: () => string; // một hàm trả về giá trị có type "string" sau khi thực thi
 }
