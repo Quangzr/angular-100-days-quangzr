ccf8e3ca3e5a3947a99b48503a6a5717d17554b8
Quangzr <82323489+Quangzr@users.noreply.github.com>
2022-12-16 15:43:45 +0700

    2 files modified, 3 added

---
+++ src/app/day12-typescript-advanced-type/day12-typescript-advanced-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-day12-typescript-advanced-type',
+  templateUrl: './day12-typescript-advanced-type.component.html',
+  styleUrls: ['./day12-typescript-advanced-type.component.css']
+})
+export class Day12TypescriptAdvancedTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
