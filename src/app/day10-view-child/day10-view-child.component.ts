49ab6372d47a48100f49805cdcb9785311db39f0
Quangzr <82323489+Quangzr@users.noreply.github.com>
2022-12-16 13:16:35 +0700

    4 files modified

--- src/app/day10-view-child/day10-view-child.component.ts
+++ src/app/day10-view-child/day10-view-child.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-day10-view-child',
   templateUrl: './day10-view-child.component.html',
-  styleUrls: ['./day10-view-child.component.css']
+  styleUrls: ['./day10-view-child.component.css'],
 })
 export class Day10ViewChildComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  isChecked = true;
 }
--- src/app/day10-view-child/toggle-day10/toggle-day10.component.ts
+++ src/app/day10-view-child/toggle-day10/toggle-day10.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-toggle-day10',
   templateUrl: './toggle-day10.component.html',
-  styleUrls: ['./toggle-day10.component.css']
+  styleUrls: ['./toggle-day10.component.css'],
 })
 export class ToggleDay10Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  @Input() checked: boolean;
+  @Output() checkedChange = new EventEmi();
+
+  toggle() {
+    this.checkedChange.emit(!this.checked);
   }
-
 }
