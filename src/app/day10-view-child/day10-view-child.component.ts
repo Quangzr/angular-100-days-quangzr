332c23e2ecad7fe763e16009ffcef54f1cd9f038
Quangzr <82323489+Quangzr@users.noreply.github.com>
2022-12-16 13:28:34 +0700

    2 files modified

--- src/app/day10-view-child/day10-view-child.component.ts
+++ src/app/day10-view-child/day10-view-child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ToggleDay10Component } from './toggle-day10/toggle-day10.component';
 
 @Component({
   selector: 'app-day10-view-child',
@@ -6,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./day10-view-child.component.css'],
 })
 export class Day10ViewChildComponent implements OnInit {
+  @ViewChild('toggleComp') toggleComp: ToggleDay10Component;
   constructor() {}
 
   ngOnInit() {}
 
   isChecked = true;
+
+  ngAfterViewInit() {
+    console.log(this.toggleComp);
+  }
 }
