433c5f30d5b29b0170993c0a447c124a0aeb7540
Quangzr <82323489+Quangzr@users.noreply.github.com>
2022-12-16 15:50:51 +0700

    5 files modified

--- src/app/day13-content-projection/day13-content-projection.component.ts
+++ src/app/day13-content-projection/day13-content-projection.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-day13-content-projection',
   templateUrl: './day13-content-projection.component.html',
-  styleUrls: ['./day13-content-projection.component.css']
+  styleUrls: ['./day13-content-projection.component.css'],
 })
 export class Day13ContentProjectionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  questions = {
+    question1: true,
+    question2: false,
+  };
 }
--- src/app/day13-content-projection/toggle-day13/toggle-day13.component.ts
+++ src/app/day13-content-projection/toggle-day13/toggle-day13.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-toggle-day13',
   templateUrl: './toggle-day13.component.html',
-  styleUrls: ['./toggle-day13.component.css']
+  styleUrls: ['./toggle-day13.component.css'],
 })
 export class ToggleDay13Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  @Input() checked = false;
+  @Output() checkedChange = new EventEmitter<boolean>();
+
+  toggle() {
+    this.checked = !this.checked;
+    this.checkedChange.emit(this.checked);
   }
-
 }
