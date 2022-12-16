a5601516137ce42eee90e8659e04ccac158ebf6c
Quangzr <82323489+Quangzr@users.noreply.github.com>
2022-12-16 13:36:46 +0700

    1 file modified

--- src/app/day10-view-child/day10-view-child.component.ts
+++ src/app/day10-view-child/day10-view-child.component.ts
@@ -7,10 +7,15 @@ import { ToggleDay10Component } from './toggle-day10/toggle-day10.component';
   styleUrls: ['./day10-view-child.component.css'],
 })
 export class Day10ViewChildComponent implements OnInit {
-  @ViewChild('toggleComp') toggleComp: ToggleDay10Component;
+  //Tên trong ngoặc phải giống với tên # ở bên html
+  @ViewChild('toggleComp', { static: true }) toggleComp: ToggleDay10Component;
   constructor() {}
 
-  ngOnInit() {}
+  //Trong ngOnInit sẽ thấy this.toggleComp undefined vì view chưa render kịp
+  // nếu muốn trong ngOnInit nhận ra thì phải static: true trong ViewChild (nhưng this.toggleComp phải ko ở trong directive)
+  ngOnInit() {
+    console.log('onInit', this.toggleComp);
+  }
 
   isChecked = true;
 
