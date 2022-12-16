dd06b628c34aed6be6d197758c9c59b644865d75
Quangzr <82323489+Quangzr@users.noreply.github.com>
2022-12-16 11:35:34 +0700

    3 files modified

--- src/app/day9-custom-2-way-binding/day9-custom-2-way-binding.component.ts
+++ src/app/day9-custom-2-way-binding/day9-custom-2-way-binding.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-day9-custom-2-way-binding',
   templateUrl: './day9-custom-2-way-binding.component.html',
-  styleUrls: ['./day9-custom-2-way-binding.component.css']
+  styleUrls: ['./day9-custom-2-way-binding.component.css'],
 })
 export class Day9Custom2WayBindingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  isChecked = true;
 }
